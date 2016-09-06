b172189203e5e08e302cf1db4302e781ee168a90
gravoo <gravoo@yahoo.com>
2016-09-06 23:07:37 +0200

    modified cmake file, introduced split for Source and Include files, removed auto generated files from repo

---
+++ Include/Graphic.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+class Graphic
+{
+public:
+    Graphic(int argc, char* argv[]);
+
+    static void ResizeFunction(int Width, int Height)
+    {
+          glViewport(0, 0, Width, Height);
+    }
+
+    static void RenderFunction(void)
+    {
+          glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+          glutSwapBuffers();
+          glutPostRedisplay();
+    }
+
+private:
+        int m_windowHandle;
+        int CurrentWidth = 800, CurrentHeight = 600, WindowHandle = 0;
+        const char* WINDOW_TITLE_PREFIX = "Particles";
+};
+
+
---
+++ Source/Graphic.cpp
@@ -0,0 +1,29 @@
+#include <Graphic.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+
+Graphic::Graphic(int argc, char* argv[])
+    : m_windowHandle(0)
+{
+  glutInit(&argc, argv);
+  glutInitContextVersion(4, 0);
+  glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+  glutInitContextProfile(GLUT_CORE_PROFILE);
+
+  glutSetOption( GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+  glutInitWindowSize(CurrentWidth, CurrentHeight);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  m_windowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+  if( m_windowHandle < 1) {
+      fprintf( stderr, "ERROR: Could not create a new rendering window.\n" );
+      exit(EXIT_FAILURE);
+  }
+  glutReshapeFunc(ResizeFunction);
+  glutDisplayFunc(RenderFunction);
+
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glutMainLoop();
+  exit(EXIT_SUCCESS);
+}
+
