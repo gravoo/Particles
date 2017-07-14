fcba6a162866fb3c1f77819ff5c0a978ffe51414
gravoo <gravoo@yahoo.com>
2017-07-14 15:26:41 +0200

    modified cmake

--- Project/Source/main.cpp
+++ Project/Source/main.cpp
@@ -3,9 +3,8 @@
 // GLFW
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <Utils.hpp>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
 int main()
 {
 
@@ -42,16 +41,4 @@ int main()
     }
     glfwTerminate();
     return 0;
-}
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, true);
-    }
 }
