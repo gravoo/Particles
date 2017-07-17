1ef095cbd2dd54cc8e6e8a62b75233840afc0e69
gravoo <gravoo@yahoo.com>
2017-07-17 22:23:45 +0200

    expand Shader class

--- Project/Include/Shader.hpp
+++ Project/Include/Shader.hpp
@@ -1,8 +1,5 @@
 #pragma once
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
 // GLFW
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -12,6 +9,15 @@ class Shader
 public:
     GLuint Program;
     Shader();
+    void compile_vertex_shader();
+    void compile_fragment_shader();
+    void compile_shaders();
     void Use();
     void set_int(const std::string &name, int value) const;
+private:
+    std::string vertex_shader;
+    std::string fragment_shader;
+    unsigned int fragment_shader_id;
+    unsigned int vertex_sader_id;
+    unsigned int shader_program;
 };
--- Project/Source/Shader.cpp
+++ Project/Source/Shader.cpp
@@ -1,12 +1,10 @@
 #include <Shader.hpp>
-#include <cassert>
 #include <Utils.hpp>
 
 Shader::Shader()
 {
-    std::cout<<"DUPA"<<std::endl;
-    std::string vertex_shader = read_file("../Shaders/SimpleShader.vertex1.glsl");
-    std::cout<<vertex_shader;
+    vertex_shader = read_file("../Shaders/SimpleShader.vertex1.glsl");
+    fragment_shader = read_file("../Shaders/SimpleShader.fragment1.glsl");
 }
 void Shader::Use()
 {
@@ -16,5 +14,53 @@ void Shader::set_int(const std::string &name, int value) const
 {
     glUniform1i(glGetUniformLocation(Program, name.c_str()), value);
 }
+void Shader::compile_vertex_shader()
+{
+    vertex_sader_id = glCreateShader(GL_VERTEX_SHADER);
+    const char* vShaderCode = vertex_shader.c_str();
+    glShaderSource(vertex_sader_id, 1, &vShaderCode, nullptr);
+    glCompileShader(vertex_sader_id);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertex_sader_id, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_sader_id, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+void Shader::compile_fragment_shader()
+{
+    fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+    const char * fShaderCode = fragment_shader.c_str();
+    glShaderSource(fragment_shader_id, 1, &fShaderCode, nullptr);
+    glCompileShader(fragment_shader_id);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader_id, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+void Shader::compile_shaders()
+{
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_sader_id);
+    glAttachShader(shader_program, fragment_shader_id);
+    glLinkProgram(shader_program);
+    int  success;
+    char infoLog[512];
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertex_sader_id);
+    glDeleteShader(fragment_shader_id);
+}
+
 
 
